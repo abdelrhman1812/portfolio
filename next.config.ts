ab634fa078d4af0592a2e25bc745fc9b75a8c805
abdelrhman1812 <abdelrhmanali1812@gmail.com>
2025-01-19 15:25:07 +0200

    add file robots

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,19 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Robots-Tag",
+            value: "index, follow",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
